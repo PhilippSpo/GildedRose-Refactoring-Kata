fac7fa742f9e3b172dbeea5c5a28dd0e4a48ceef
Philipp Sporrer <philipp.sporrer@celebrate.company>
2023-10-23 23:50:48 +0200

    refactor: apply if only at start rule

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -24,14 +24,14 @@ export class GildedRose {
       }
       this.updateItemQuality(this.items[i]);
       this.decreaseItemSellIn(this.items[i]);
-      this.updateItemQualityForNegativeSellIn(this.items[i]);
+      this.updateItemQualityAfterDecreasingSellIn(this.items[i]);
     }
 
     return this.items;
   }
 
   private isSulfuras(item: Item) {
-    return item.name == "Sulfuras, Hand of Ragnaros"
+    return item.name == "Sulfuras, Hand of Ragnaros";
   }
 
   private updateItemQuality(item: Item) {
@@ -68,20 +68,22 @@ export class GildedRose {
     item.sellIn = item.sellIn - 1;
   }
 
-  private updateItemQualityForNegativeSellIn(item: Item) {
+  private updateItemQualityAfterDecreasingSellIn(item: Item) {
     if (item.sellIn < 0) {
-      if (item.name == "Aged Brie") {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      } else {
-        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-          item.quality = item.quality - item.quality;
-        } else {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1;
-          }
-        }
+      this.updateItemQualityForNegativeSellIn(item);
+    }
+  }
+
+  private updateItemQualityForNegativeSellIn(item: Item) {
+    if (item.name == "Aged Brie") {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+    } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+      item.quality = item.quality - item.quality;
+    } else {
+      if (item.quality > 0) {
+        item.quality = item.quality - 1;
       }
     }
   }
