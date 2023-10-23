3f92193ace339e9850f7bae3148acaadcb942a35
Philipp Sporrer <philipp.sporrer@celebrate.company>
2023-10-23 23:44:04 +0200

    refactor: extract value gaining item behavior

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -39,24 +39,27 @@ export class GildedRose {
       item.name == "Aged Brie" ||
       item.name == "Backstage passes to a TAFKAL80ETC concert"
     ) {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
-      }
+      this.updateValueGainingItemQuality(item);
     } else {
       if (item.quality > 0) {
         item.quality = item.quality - 1;
+      }
+    }
+  }
+  private updateValueGainingItemQuality(item: Item) {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
+      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.sellIn < 11) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+        if (item.sellIn < 6) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
       }
     }
   }
